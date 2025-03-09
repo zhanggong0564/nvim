3cb5a259e39e88b8666321aa6b7a05da5c78512e
张弓 <gongzhang4@iflytek.com>
2025-03-09 14:50:59 +0800

    增加快捷键映射

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+local map = vim.keymap.set
+
+map("i", "jj", "<esc>", { desc = "esc" })
+map("n", "J", "5j", { desc = "5j" })
+map("n", "K", "5k", { desc = "5k" })
+
+map("v", "J", "5j", { desc = "5j" })
+map("v", "K", "5k", { desc = "5k" })
+
+map("n", "H", "^", { desc = "left line" })
+map("n", "L", "g_", { desc = "right line" })
+
+map("v", "H", "^", { desc = "left line" })
+map("v", "L", "g_", { desc = "right line" })
+
+map("o", "H", "^", { desc = "left line" })
+map("o", "L", "g_", { desc = "right line" })
